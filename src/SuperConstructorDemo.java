2b73e2a91bfb79408d7e64d0a70fb41b3f382f0d
govind <govind.g.coder@gmail.com>
2026-02-13 21:51:16 +0530

    inital commit

--- src/SuperConstructorDemo.java
+++ src/SuperConstructorDemo.java
@@ -1,17 +1,25 @@
 class Person {
     // TODO: Create a constructor Person()
     // Print "Person class"
+    public Person() {
+        System.out.println("Person class");
+    }
 }
 
 class Student extends Person {
     // TODO: Create a constructor Student()
     // Call super() first (this is implicit, but explicit is good practice)
     // Print "Student class"
+    public Student() {
+        super();
+        System.out.print("Student class");
+    }
 }
 
 public class SuperConstructorDemo {
     public static void main(String[] args) {
         // TODO: Create an object of Student class
         // Creating the object will automatically invoke constructors
+        Student st = new Student();
     }
 }
